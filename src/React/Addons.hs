79d4ec6c35b587dd2ade987d4a4b44b5741dcf32
Ian Duncan <ian@iankduncan.com>
2016-08-06 14:09:19 +0900

    Update React version and add option to use react with addons or not

---
+++ src/React/Addons.hs
@@ -0,0 +1,19 @@
+module React.Addons where
+
+import React
+
+{-
+data TransitionGroup
+  { transitionName         :: Either JSString [(JSString, JSString)]
+  , transitionEnterTimeout :: Maybe Int
+  , transitionLeaveTimeout :: Maybe Int
+  , transitionEnter        :: Maybe Bool
+  , transitionLeave        :: Maybe Bool
+  , transitionAppear       :: Maybe Bool
+  }
+
+foreign import
+
+reactCSSTransitionGroup :: TransitionGroup -> [ReactElement] -> ReactElement
+re
+-}
