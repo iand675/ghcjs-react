58ee65ef6dc55654cafe186eea1e005a466890b7
Ian Duncan <ian@iankduncan.com>
2016-08-08 10:46:47 +0900

    Add animation addon and DOM utils

--- src/React/Addons.hs
+++ src/React/Addons.hs
@@ -1,9 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
 module React.Addons where
 
+import qualified Data.Foldable as F
+import Data.Maybe (catMaybes)
+import Data.JSString (JSString)
+import GHCJS.Types
 import React
+import React.DOM (className_)
 
-{-
-data TransitionGroup
+foreign import javascript unsafe "React.addons.CSSTransitionGroup" js_cssTransitionGroup :: ReactClass OnlyAttributes
+
+cssTransition :: Props OnlyAttributes -> Maybe (Array ReactElement) -> ReactElement
+cssTransition = runFactory' fact
+  where
+    fact = createFactory js_cssTransitionGroup
+{-# NOINLINE cssTransition #-}
+
+cssTransitionGroup :: (Foldable elems) => JSString -> TransitionGroup -> elems ReactElement -> ReactElement
+cssTransitionGroup cl g es = cssTransition builtProps $ if Prelude.null es
+  then Nothing
+  else Just $ array $ F.toList es
+  where
+    builtProps = buildProps $ catMaybes
+      [ Just (className_ cl)
+      , Just (PropName "transitionName" .: (either jsval (jsval . buildProps . map (uncurry (\k v -> PropName k .: v))) $ transitionName g))
+      , (\x -> PropName "transitionEnterTimeout" .: x) <$> transitionEnterTimeout g
+      , (\x -> PropName "transitionLeaveTimeout" .: x) <$> transitionLeaveTimeout g
+      , (\x -> PropName "transitionEnter" .: x) <$> transitionEnter g
+      , (\x -> PropName "transitionLeave" .: x) <$> transitionLeave g
+      , (\x -> PropName "transitionAppear" .: x) <$> transitionAppear g
+      ]
+
+data TransitionGroup = TransitionGroup
   { transitionName         :: Either JSString [(JSString, JSString)]
   , transitionEnterTimeout :: Maybe Int
   , transitionLeaveTimeout :: Maybe Int
@@ -12,8 +40,8 @@ data TransitionGroup
   , transitionAppear       :: Maybe Bool
   }
 
-foreign import
+simpleTransition :: JSString -> TransitionGroup
+simpleTransition str = TransitionGroup (Left str) Nothing Nothing Nothing Nothing Nothing
 
-reactCSSTransitionGroup :: TransitionGroup -> [ReactElement] -> ReactElement
-re
--}
+complexTransition :: [(JSString, JSString)] -> TransitionGroup
+complexTransition strs = TransitionGroup (Right strs) Nothing Nothing Nothing Nothing Nothing
---
+++ src/React/DOM/Utils.hs
@@ -0,0 +1,15 @@
+module React.DOM.Utils
+  ( eventFormValue
+  ) where
+
+import Control.Monad.Trans
+import GHCJS.DOM.EventTarget
+import GHCJS.DOM.HTMLInputElement
+import GHCJS.DOM.Types
+
+-- | Form targets all return strings, so make it simpler to get at the string without having
+-- to reimport the numerous GHCJS.DOM modules
+eventFormValue :: (MonadIO m, FromJSString string) => EventTarget -> m (Maybe string)
+eventFormValue t = liftIO $ do
+  let el = castToHTMLInputElement t
+  getValue el
