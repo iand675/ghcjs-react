fd6764f7764c4ff193651e401c02e90eb8a151d2
Ian Duncan <ian@iankduncan.com>
2016-08-10 07:17:22 +0900

    castToHTMLInputElement is insufficient for my nefarious plans

--- src/React/DOM/Utils.hs
+++ src/React/DOM/Utils.hs
@@ -6,10 +6,11 @@ import Control.Monad.Trans
 import GHCJS.DOM.EventTarget
 import GHCJS.DOM.HTMLInputElement
 import GHCJS.DOM.Types
+import Unsafe.Coerce
 
 -- | Form targets all return strings, so make it simpler to get at the string without having
 -- to reimport the numerous GHCJS.DOM modules
 eventFormValue :: (MonadIO m, FromJSString string) => EventTarget -> m (Maybe string)
 eventFormValue t = liftIO $ do
-  let el = castToHTMLInputElement t
-  getValue el
+  let el = unsafeCoerce t
+  getValue (el :: HTMLInputElement)
